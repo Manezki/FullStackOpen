2f5087a79d7e3a82ac711f083bc7bca5fa083d8f
Manezki <manezki@gmail.com>
2021-01-18 20:31:48 +0100

    Exercise 2.11

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Contacts from "./components/Contacts";
 import NewContactForm from "./components/NewContactForm";
 
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ searchState, setSearchState ] = useState('')
+
+  useEffect( () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then( (response) => {
+
+        // Prevent duplicate entries in the contacts
+        const checkUpSet = new Set(persons.map(p => p.name))
+        const newPersons = [...persons].concat(response.data.filter(p => !checkUpSet.has(p.name)))
+        setPersons(newPersons)
+        
+      })
+    // Run only on first render, so keeping the dependencies empty
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleSubmit = (event) => {
     event.preventDefault()
