ffbd7c85f9b14ff405c7586d424a85a70d292812
Manezki <manezki@gmail.com>
2021-06-03 14:58:32 +0200

    Ex8.8-12: Feat: Init library frontend

---
+++ part-8/library-frontend/src/components/SetBirthyear.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+
+import { useLazyQuery, useMutation } from "@apollo/client"
+
+import { SET_BIRTHYEAR, ALL_AUTHORS } from "../queries"
+
+const SetBirthYear = () => {
+  const [author, setAuthor] = useState("")
+  const [birthyear, setBirthyear] = useState("")
+
+  const [ mut_setBirthyear ] = useMutation(SET_BIRTHYEAR)
+  const [ getAuthors, result ] = useLazyQuery(ALL_AUTHORS)
+
+  const fetchAuthors = () => {
+    if (!result.loading && result.data) {
+      return
+    }
+    getAuthors()
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    mut_setBirthyear({
+      variables: { name: author, setBornTo: Number(birthyear) },
+      refetchQueries: [ { query: ALL_AUTHORS } ]
+    })
+
+    setAuthor("")
+    setBirthyear("")
+  }
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          Author
+          <select
+            name="author"
+            value={author}
+            required={true}
+            onChange={({ target }) => setAuthor(target.value)}
+            onMouseOver={() => fetchAuthors()}
+          >
+            {(!result.loading && result.data)
+              ? result.data.allAuthors.map((author) => {
+                return <option value={author.name} key={author.id}>{author.name}</option>
+              })
+              : <option value={"empty"}>--Select an author--</option>
+            }
+          </select>
+          {/* <input
+            value={author}
+            required={true}
+            onChange={({ target }) => setAuthor(target.value)}
+          /> */}
+        </div>
+        <div>
+          Birthyear
+          <input
+            type='number'
+            value={birthyear}
+            onChange={({ target }) => setBirthyear(target.value)}
+          />
+        </div>
+        <button type='submit'>Set birthyear</button>
+      </form>
+    </div>
+  )
+}
+
+export default SetBirthYear
