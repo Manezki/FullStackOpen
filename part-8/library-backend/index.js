c0db7a04fa5fcef4153aa54cc8cb3dc0301eef5d
Manezki <manezki@gmail.com>
2021-06-04 17:14:19 +0200

    Ex8.13-16: Feat: Connect to MongoDB & Add Authentication

---
+++ part-8/library-backend/index.js
@@ -0,0 +1,103 @@
+require("dotenv").config()
+
+const { ApolloServer, gql } = require("apollo-server")
+const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
+
+const resolvers = require("./services/resolvers")
+const User = require("./models/user")
+
+const typeDefs = gql`
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]
+    allAuthors: [Author!]
+    me: User
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token  
+  }
+`
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+).then(() => {
+  console.log("Connected to MongoDB")
+}).catch((error) => {
+  console.error(`MongoDB connection failed. ${error.message}`)
+})
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.TOKEN_SECRET,
+      )
+
+      const currentUser = await User.findById(decodedToken.id)
+
+      return { currentUser }
+    }
+    return {}
+  },
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
---
+++ part-8/library-backend/services/resolvers.js
@@ -0,0 +1,114 @@
+require("dotenv").config()
+const { UserInputError } = require("apollo-server")
+const jwt = require("jsonwebtoken")
+const Book = require("../models/book")
+const Author = require("../models/author")
+const User = require("../models/user")
+
+const resolvers = {
+  Query: {
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: (root, args) => {
+      try {
+        const filteredByGenre = (args.genre)
+          ? Book.find({ genres: { $in: [args.genre] } })
+          : Book.find({})
+
+        return filteredByGenre
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+    },
+    allAuthors: () => Author.find({}),
+    me: (root, args, context) => context.currentUser,
+  },
+  Author: {
+    bookCount: async (root) => {
+      const authorBooks = await Book.find({ author: root.id })
+      return authorBooks.length
+    },
+  },
+  Mutation: {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new UserInputError("Login required to add a new book. See login")
+      }
+
+      if (!args.author) {
+        throw new UserInputError("Author name is required", {
+          invalidArgs: args,
+        })
+      }
+
+      try {
+        const author = await Author.findOne({ name: { $regex: args.author, $options: "i" } })
+
+        const newAuthor = author || await new Author({ name: args.author }).save()
+
+        let newBook = await new Book({
+          title: args.title,
+          author: newAuthor.id,
+          published: Number(args.published),
+          genres: args.genres,
+        }).save()
+
+        newBook = await newBook.populate("author").execPopulate()
+
+        return newBook
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+    },
+    editAuthor: (root, args, context) => {
+      if (!context.currentUser) {
+        throw new UserInputError("Login required to add a new book. See login")
+      }
+
+      if (!args.name || !args.setBornTo) {
+        throw new UserInputError("Author name and setBornTo parameters required", {
+          invalidArgs: args,
+        })
+      }
+
+      return Author.findOneAndUpdate({ name: args.name }, { born: args.setBornTo }, { new: true })
+    },
+
+    createUser: (root, args) => {
+      try {
+        const user = new User({
+          username: args.username,
+          favoriteGenre: args.favoriteGenre,
+        })
+        return user.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== "password") {
+        throw new UserInputError("Wrong username or password")
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id, // eslint-disable-line
+      }
+
+      const token = await jwt.sign(userForToken, process.env.TOKEN_SECRET)
+
+      return { value: token }
+    },
+  },
+}
+
+module.exports = resolvers
